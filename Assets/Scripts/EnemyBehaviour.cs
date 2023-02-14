b869d7b67ab95860c93b31d048ecb6f80b6d1207
Braxtony97 <renat_samigullin_97@mail.ru>
2023-02-14 23:18:58 +0500

    Fixed some bugs

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -13,39 +13,95 @@ public class EnemyBehaviour : MonoBehaviour
     private Animation anim;
     public int health = 100;
 
+    public double impactTime = 0.33;
+    bool impacted;
+    private Fighter opponent;
+    public int damage;
+
+
     void Start()
     {
         anim = GetComponent<Animation>();
+        opponent = player.GetComponent<Fighter>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inRange())
+        if (!isDead())
         {
-            chase();
+            if (!inRange())
+            {
+                chase();
+            }
+            else
+            {
+                //anim.CrossFade("idle");
+                anim.Play("attack");
+                attack();
+                if (anim["attack"].time > anim["attack"].length * 0.9)
+                //что бы атака вызывалась еще раз только после 90% продолжительности атаки
+                //но будет вызыватьс€ каждый кадр до 100% продолжительности
+                //поэтому в самом методе attack() нужно прописать условие
+                //что наносить урон только от impactTime до 90%
+                {
+                    impacted = false;
+                    //если (impacted = false;), то метод attack() 
+                    //вызоветс€ только 1 раз
+                }
+            }
         }
         else
         {
-            anim.CrossFade("idle");
+            dieMethod();
         }
-        Debug.Log(health);
     }
 
     public void GetHit(int damage)
     {
         health = health - damage;
-        if (health < 0)
+        /*if (health < 0)
         {
             Destroy(gameObject);
-        }
+        }*/
     }
 
+    void attack()
+    {
+        if (anim.IsPlaying("attack") && !impacted)
+        //если анимаци€ attack проигрываетс€ » impacted = false;, то выполн€етс€ тело
+        {
+            if (anim["attack"].time > anim["attack"].length * impactTime && anim["attack"].time < anim["attack"].length * 0.9)
+            // если врем€ атаки больше, чем продолжительность атаки (100%) * импакт“айм(врем€ атаки) (0.33) 
+            // » врем€ атаки меньше, чем 90% продолжительности атаки 
+            {
+                    opponent.getHit(damage);
+                    // у opponent вызываем метод getHit и передаем ему damage
+                    impacted = true;
+                    //если не поставить (impacted = true;) - то метод attack() будет 
+                    //вызыватьс€ каждый кадр (в Update())
+                
+            }
+
+        }
+    }
     bool inRange()
     {
         return (Vector3.Distance(transform.position, player.position) < range);
     }
-
+    void dieMethod()
+    {
+        anim["die"].wrapMode = WrapMode.ClampForever;
+        anim.Play("die");
+        if (anim["die"].time > anim["die"].length * 0.9)
+        {
+            Destroy(gameObject);
+        }
+    }
+    bool isDead()
+    {
+        return (health <= 0) ;
+    }
     void chase()
     {
         //Quaternion newRotation = Quaternion.LookRotation(transform.position - player.position);
--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+
 
 public class Fighter : MonoBehaviour
 {
@@ -9,14 +12,20 @@ public class Fighter : MonoBehaviour
     public int damage;
     public double impactTime;
     private bool impacted;
+    public float range;
+    public int health;
+
+    bool starterd;
+    bool ended;
     void Start()
     {
-        anim = GetComponent<Animation>();      
+        anim = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(health + " Player"); 
         
         if (Input.GetKey(KeyCode.Space))
         {
@@ -24,14 +33,15 @@ public class Fighter : MonoBehaviour
             {
                 anim.Play("attack");
                 transform.LookAt(opponent.transform.position);
-                ClickToMove.attack = true;
+                ClickToMove.attack = true; // при одиночном нажатии на space - не будет ударять (только если держать)
+                // Нажали space - произошел удар, при этом в классе ClickToMove мы остановились (начинает играть анимация idle) - ведь мы не нажимает ЛКМ
             }
         }
-        if (!anim.IsPlaying("attack")){
-            ClickToMove.attack = false;
+        if (anim["attack"].time > 0.9 * anim["attack"].length){
+            ClickToMove.attack = false; // для того, что бы после удара можно было управлять персонажем дальше
             impacted = false;
         }
-      
+        die();
         impact();
     }
 
@@ -39,13 +49,54 @@ public class Fighter : MonoBehaviour
     {
         if (opponent != null && anim.IsPlaying("attack") && !impacted)
         {
-            if (anim["attack"].time > anim["attack"].length * impactTime)
+            if ((anim["attack"].time > anim["attack"].length * impactTime) &&  (anim["attack"].time < anim["attack"].length * 0.9))
             {
-                opponent.GetComponent<EnemyBehaviour>().GetHit(damage);
-                impacted = true;
+                if (inRange())
+                {
+                    
+                    opponent.GetComponent<EnemyBehaviour>().GetHit(damage);
+                    impacted = true;
+                } 
             }
         }
     }
 
+    bool inRange()
+    {
+        return Vector3.Distance(transform.position, opponent.transform.position) <= range;
+    }
+
+    public void getHit(int damage)
+    {
+        health = health - damage;
+        if (health < 0)
+        {
+            health = 0;
+        }
+        //если не добавить блок с if, то здоровье будет уменьшаться дальше ( в минус)
+       
+    } 
+    public bool isDead()
+    {
+        return (health <= 0);
+    }
+    void die()
+    {
+        if (isDead() && !ended)
+        // если у Игрока закончилось здоровье
+        {
+            if (!starterd)
+            {
+                anim.Play("die");
+                
+                starterd = true;
+            } 
+            if (starterd && anim.IsPlaying("die"))
+            {
+                Debug.Log("Dead");
+                ended = true;
+            }
+        }
+    }
 }
 
